3a2098d841361a12874366a558aa70824158fa78
Victor Dibia <victordibia@microsoft.com>
2022-10-27 21:49:45 -0700

    remove autocast, add dtype parameter and revision parameter

--- peacasso/generator.py
+++ peacasso/generator.py
@@ -1,7 +1,7 @@
 from dataclasses import asdict
 from torch import autocast
 from PIL import Image
-from typing import List
+from typing import List, Optional
 # from diffusers import StableDiffusionPipeline
 
 import os
@@ -20,23 +20,25 @@ class ImageGenerator:
         model: str = "CompVis/stable-diffusion-v1-4",
         token: str = os.environ.get("HF_API_TOKEN"),
         cuda_device: int = 0,
+        revision: str = "fp16",
+        torch_dtype: Optional[torch.FloatTensor] = torch.float32
     ) -> None:
 
         assert token is not None, "HF_API_TOKEN environment variable must be set."
         self.device = f"cuda:{cuda_device}" if torch.cuda.is_available() else "cpu"
         self.pipe = StableDiffusionPipeline.from_pretrained(
             model,
-            revision="fp16",
-            torch_dtype=torch.float16,
+            revision=revision,
+            torch_dtype=torch_dtype,
             use_auth_token=token,
         ).to(self.device)
 
     def generate(self, config: GeneratorConfig) -> Image:
         """Generate image from prompt"""
-        config.prompt = [config.prompt] * config.num_images
-        with autocast("cuda" if torch.cuda.is_available() else "cpu"):
-            results = self.pipe(**asdict(config))
-        return results
+        config.prompt = [config.prompt]
+        # with autocast("cuda" if torch.cuda.is_available() else "cpu"):
+        #     results = self.pipe(**asdict(config))
+        return self.pipe(**asdict(config))
 
     def list_cuda(self) -> List[int]:
         """List available cuda devices
