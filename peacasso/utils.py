1da313cd56658019e7fd857e41c474bf28e7b896
Victor Dibia <victordibia@microsoft.com>
2022-10-31 00:19:43 -0700

    add utils for pil to b64 to send images over socket

--- peacasso/utils.py
+++ peacasso/utils.py
@@ -1,9 +1,14 @@
+from ast import Str
 import base64
 import json
 from typing import Any, List
 import os
 from PIL import Image
 import io
+
+from traitlets import Float
+
+from peacasso.pipelines import StableDiffusionPipeline
 
 
 def get_dirs(path: str) -> List[str]:
@@ -16,9 +21,18 @@ def base64_to_pil(base64_string: str) -> Image:
     img_bytes = base64.b64decode(base64_string)
     img = Image.open(io.BytesIO(img_bytes))
     # print number of channels
-    print(img.mode)
     mask = None
     if img.mode == "RGBA":
         mask = img.getchannel("A")
-        img = img.convert("RGB") 
+        img = img.convert("RGB")
     return img, mask
+
+
+def pil_to_base64(img: Image) -> str:
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format="PNG")
+    img.close()
+    return base64.b64encode(img_byte_arr.getvalue()).decode("utf-8")
+
+
+    
